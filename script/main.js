4551e37cfa2473dd02714312386d66d6349e56d7
tamamu <tamamu.1r1s@gmail.com>
2017-01-03 23:36:42 +0900

    add commands: whoami github ?

--- script/main.js
+++ script/main.js
@@ -27,6 +27,16 @@ function println(stream, str, fg = fgColor, bg = bgColor) {
   }
 }
 
+function printURL(stream, href, fg = fgColor, bg = bgColor) {
+  let node = document.createElement('a');
+  node.style.color = fg;
+  node.style.backgroundColor = bg;
+  node.textContent = href;
+  node.href = href;
+  node.target = '_blank';
+  stream.appendChild(node);
+}
+
 function print2017() {
   println(stdout, 'A Happy New Year!\n\
   _______  ________    _____   ________  \n\
@@ -38,14 +48,37 @@ function print2017() {
     \\|_______|\\|_______|    \\|__||__|/   \n');
 }
 
+function whoami() {
+  println(stdout, 'Tamamu a.k.a. Hidetoshi ITO');
+  println(stdout, 'Age:\t\t20');
+  println(stdout, 'Country:\tJapan');
+  println(stdout, 'Enroll:\t\tIwate Prefectural University');
+}
+
+function github() {
+  print(stdout, 'My Github: ', 'yellow');
+  printURL(stdout, 'https://github.com/tamamu', 'green');
+  println(stdout, '');
+}
+
 function printPrompt() {
   print(stdout, '$ ');
 }
 
+function selectLast(editableElement) {
+  let range = document.createRange();
+  range.selectNodeContents(editableElement);
+  range.collapse(false);
+  let sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+}
+
 class Command {
-  constructor(execName, callback) {
+  constructor(execName, callback, description = '') {
     this.execName = execName;
     this.callback = callback;
+    this.description = description;
   }
   run() {
     this.callback();
@@ -56,26 +89,65 @@ class Command {
 class Shell {
   constructor() {
     this.bin = [];
+    this.history = [];
+    this.currentLine = 0;
   }
   regist(cmd) {
     this.bin[this.bin.length] = cmd;
   }
+  help() {
+    println(stdout, 'irish: commands:');
+    for (let i=0; i<this.bin.length; i++) {
+      let cmd = this.bin[i];
+      println(stdout, cmd.execName+' - '+cmd.description);
+    }
+    printPrompt();
+  }
+  previous() {
+    this.currentLine -= 1;
+    if (this.currentLine < 0)
+      this.currentLine = 0;
+    return this.history[this.currentLine];
+  }
+  next() {
+    this.currentLine += 1;
+    if (this.currentLine >= this.history.length) {
+      this.currentLine = this.history.length;
+      return '';
+    } else {
+      return this.history[this.currentLine];
+    }
+  }
+  appendHistory(input) {
+    let execName = input.trim();
+    if (execName === '') {
+      return;
+    } else {
+      this.history.push(input);
+      this.currentLine = this.history.length;
+    }
+  }
   exec(input) {
-    if (input === '') {
+    let execName = input.trim();
+    if (execName === '') {
       printPrompt();
+      return;
+    }
+    if (execName === '?') {
+      this.help();
       return;
     }
 
     let found = false;
     for (let cmd of this.bin) {
-      if (cmd.execName === input) {
+      if (cmd.execName === execName) {
         found = true;
         cmd.run();
         break;
       }
     }
     if (!found) {
-      println(stdout, "irish: command not found: " + input);
+      println(stdout, "irish: command not found: " + execName);
       printPrompt();
     }
   }
@@ -84,7 +156,9 @@ class Shell {
 window.onload = () => {
   stdout = document.getElementById('stdout');
   let shell = new Shell();
-  shell.regist(new Command('greeting', print2017));
+  shell.regist(new Command('greeting', print2017, 'Print greeting message'));
+  shell.regist(new Command('whoami', whoami, 'Display my profile'));
+  shell.regist(new Command('github', github, 'Show my github URL'));
   let terminal = document.getElementById('terminal');
   terminal.onclick = (e) => {
     stdin.focus();
@@ -93,12 +167,24 @@ window.onload = () => {
   stdin.onkeydown = (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
+      shell.appendHistory(stdin.textContent);
       println(stdout, stdin.textContent);
       shell.exec(stdin.textContent);
       stdin.textContent = "";
+      terminal.scrollTop = terminal.scrollTopMax;
+    } else if (e.keyCode === 38) { // ↑
+      e.preventDefault();
+      stdin.textContent = shell.previous();
+      selectLast(stdin);
+      terminal.scrollTop = terminal.scrollTopMax;
+    } else if (e.keyCode === 40) { // ↓
+      e.preventDefault();
+      stdin.textContent = shell.next();
+      selectLast(stdin);
+      terminal.scrollTop = terminal.scrollTopMax;
     }
   }
   stdin.focus();
 
-  shell.exec("greeting");
+  shell.exec('?');
 }
